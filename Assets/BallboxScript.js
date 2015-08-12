8669695886f0e42e6fe599cb480de87c01775f20
mediaboxes <mediaboxes@gmail.com>
2015-08-12 12:36:59 +0900

    サンプル

---
+++ Assets/BallboxScript.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var ball : Transform;
+var n:int = 0;
+
+function Update () {
+	if(Input.GetButtonUp("Jump")){
+		Instantiate(ball,transform.position,transform.rotation);
+		n++;
+	}
+	if(n > 10){
+		Application.LoadLevel("GameOver");
+	}
+}
---
+++ Assets/GameOverScript.js
@@ -0,0 +1,7 @@
+﻿#pragma strict
+
+var style:GUIStyle;
+
+function OnGUI () {
+	GUI.Label(Rect(10,10,200,50),"GameOver",style);
+}
